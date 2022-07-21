f66cd2901fab2a4278f5f4affbbde6b4b37601ac
Nnaji Ugochukwu Lawrence <ugochukwulawrencennaji@gmail.com>
2022-07-20 21:33:33 -0700

    recurssion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,13 +1,65 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+* substring_match - check if a substring after wildcard matches s1
+* @s1: one string
+* @s2: one string
+* @after_wldcd: placeholder for position right after wildcard
+* Return: 1 if matched, 0 if not
+*/
+
+int substring_match(char *s1, char *s2, char *after_wldcd)
 {
-	return (write(1, &c, 1));
+
+	if (*s1 == '\0' && *s2 == '\0')
+
+		return (1);
+
+	if (*s1 == '\0' && *s2 == '*')
+
+		return (substring_match(s1, s2 + 1, s2 + 1));
+
+	if (*s1 == '\0' && *s2 != '\0')
+
+		return (0);
+
+	if (*s2 == '*')
+
+		return (substring_match(s1, s2 + 1, s2 + 1));
+
+	if (*s1 == *s2)
+
+		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
+
+	else
+
+		return (substring_match(s1 + 1, after_wldcd, after_wldcd));
+
+}
+
+/**
+* wildcmp - compare if string with wildcard mattches
+* @s1: one string
+* @s2: one string
+* Return: 1 if matched, 0 if not
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+
+		return (1);
+
+	if (*s1 == *s2)
+
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	else if (*s2 == '*')
+
+		return (substring_match(s1, (s2 + 1), (s2 + 1)));
+
+	else
+
+		return (0);
+
 }
